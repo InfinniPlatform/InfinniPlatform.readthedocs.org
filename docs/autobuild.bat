ff91e4d9103896e3bc9126740e442cdd55f67bed
AlexMAS <as.mezhov@gmail.com>
2016-09-12 21:11:46 +0500

    MC-4363: Try to fix import extension sphinxcontrib.httpdomain

--- docs/autobuild.bat
+++ docs/autobuild.bat
@@ -1,4 +1,5 @@
 @echo To install sphinx-autobuild run command:
+@echo $ pip install sphinx_rtd_theme
 @echo $ pip install sphinx-autobuild
 @echo $ pip install sphinx sphinxcontrib-httpdomain
 make livehtml
